2a5a1a72ace0617a274f76c8f4c526c1bf4d8b47
Cédric Belin <cedric@belin.io>
2019-04-14 11:58:13 +0200

    Updated the TypeDoc settings

--- doc/typedoc.js
+++ doc/typedoc.js
@@ -1,8 +1,10 @@
+const {join} = require('path');
+
 module.exports = {
   excludePrivate: true,
   gaID: process.env.GOOGLE_ANALYTICS_ID,
   gitRevision: 'master',
   mode: 'modules',
   name: 'SAB Rich Text Editor',
-  out: 'doc/api'
+  out: join(__dirname, 'api')
 };
