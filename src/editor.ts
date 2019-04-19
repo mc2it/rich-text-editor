2cbf6f330ce9b9de4daf3a87adf1de5f3f6a1652
Cédric Belin <cedric@belin.io>
2019-04-20 00:47:02 +0200

    Enabled the font color and background color features

--- src/editor.ts
+++ src/editor.ts
@@ -1,46 +1,50 @@
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
+// @ts-ignore
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
+// @ts-ignore
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Image from '@ckeditor/ckeditor5-image/src/image';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Link from '@ckeditor/ckeditor5-link/src/link';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import List from '@ckeditor/ckeditor5-list/src/list';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import Table from '@ckeditor/ckeditor5-table/src/table';
-// @ts-ignore: no typings available for this package.
+// @ts-ignore
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
 import {JsonMap} from './map';
@@ -54,6 +58,7 @@ export class RichTextEditor extends ClassicEditor {
     Autoformat,
     Bold, Italic, Strikethrough, Underline,
     Essentials, // Includes: Clipboard, Enter, ShiftEnter, Typing and Undo.
+    FontBackgroundColor, FontColor,
     Heading,
     Image, ImageCaption, ImageStyle, ImageToolbar,
     Link,
@@ -80,6 +85,7 @@ export class RichTextEditor extends ClassicEditor {
     toolbar: [
       'heading', '|',
       'bold', 'italic', 'underline', 'strikethrough', 'link', '|',
+      'fontColor', 'fontBackgroundColor', '|',
       'undo', 'redo', '|',
       'bulletedList', 'numberedList', '|',
       'alignment:left', 'alignment:center', 'alignment:right', '|',
