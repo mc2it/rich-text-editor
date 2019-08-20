e6e7362ec4ecea31e103cf1519448ae7ba42102a
Cédric Belin <cedric@belin.io>
2019-08-20 18:55:56 +0200

    Finalized the TS port

--- src/editor.ts
+++ src/editor.ts
@@ -4,8 +4,7 @@ import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
-import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
-import RemoveFormatLinks from '@ckeditor/ckeditor5-remove-format/src/removeformatui';
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
@@ -21,62 +20,54 @@ import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+import RemoveFormatLinks from '@ckeditor/ckeditor5-remove-format/src/removeformatui';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
 /** An implementation of a rich text editor. */
 export class RichTextEditor extends ClassicEditor {
 
-  /**
-   * The set of supported plug-ins.
-   * @type {Array}
-   */
-  static get builtinPlugins() {
-    return [
-      Alignment,
-      Autoformat,
-      Bold, Italic, Strikethrough, Underline,
-      Essentials,
-      FontBackgroundColor, FontColor,
-      Heading,
-      Image, ImageCaption, ImageStyle, ImageToolbar,
-      Link,
-      List,
-      MediaEmbed, MediaEmbedToolbar,
-      Paragraph,
-      PasteFromOffice,
-      RemoveFormat, RemoveFormatLinks,
-      Table, TableToolbar
-    ];
-  }
+  /** The set of supported plug-ins. */
+  static readonly builtinPlugins: Plugin[] = [
+    Alignment,
+    Autoformat,
+    Bold, Italic, Strikethrough, Underline,
+    Essentials,
+    FontBackgroundColor, FontColor,
+    Heading,
+    Image, ImageCaption, ImageStyle, ImageToolbar,
+    Link,
+    List,
+    MediaEmbed, MediaEmbedToolbar,
+    Paragraph,
+    PasteFromOffice,
+    RemoveFormat, RemoveFormatLinks,
+    Table, TableToolbar
+  ];
 
-  /**
-   * The default editor settings.
-   * @type {object}
-   */
-  static get defaultConfig() {
-    return {
-      alignment: {
-        options: ['left', 'center', 'right']
-      },
-      image: {
-        styles: ['full', 'alignLeft', 'alignCenter', 'alignRight'],
-        toolbar: ['imageStyle:full', 'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight']
-      },
-      language: 'fr',
-      table: {
-        contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
-      },
-      toolbar: [
-        'heading', '|',
-        'bold', 'italic', 'underline', 'strikethrough', 'link', '|',
-        'fontColor', 'fontBackgroundColor', '|',
-        'removeFormat', '|',
-        'undo', 'redo', '|',
-        'bulletedList', 'numberedList', '|',
-        'alignment:left', 'alignment:center', 'alignment:right', '|',
-        'insertTable', 'mediaEmbed'
-      ]
-    };
-  }
+  /** The default editor settings. */
+  static readonly defaultConfig: Record<string, any> = {
+    alignment: {
+      options: ['left', 'center', 'right']
+    },
+    image: {
+      styles: ['full', 'alignLeft', 'alignCenter', 'alignRight'],
+      toolbar: ['imageStyle:full', 'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight']
+    },
+    language: 'fr',
+    table: {
+      contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+    },
+    toolbar: [
+      'heading', '|',
+      'bold', 'italic', 'underline', 'strikethrough', 'link', '|',
+      'fontColor', 'fontBackgroundColor', '|',
+      'removeFormat', '|',
+      'undo', 'redo', '|',
+      'bulletedList', 'numberedList', '|',
+      'alignment:left', 'alignment:center', 'alignment:right', '|',
+      'insertTable', 'mediaEmbed'
+    ]
+  };
 }
