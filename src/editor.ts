4021280d23cc7e53fd2112d90ffb6ad066ef1ebb
Cédric Belin <cedric@belin.io>
2019-08-29 12:30:45 +0200

    Code formatting

--- src/editor.ts
+++ src/editor.ts
@@ -1,5 +1,3 @@
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
@@ -7,6 +5,7 @@ import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
 import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,3 @@
-declare module '@ckeditor/ckeditor5-editor-classic/src/classiceditor' { export default class ClassicEditor {} }
-
 declare module '@ckeditor/ckeditor5-alignment/src/alignment' { export default class Alignment {} }
 declare module '@ckeditor/ckeditor5-autoformat/src/autoformat' { export default class Autoformat {} }
 declare module '@ckeditor/ckeditor5-basic-styles/src/bold' { export default class Bold {} }
@@ -7,8 +5,7 @@ declare module '@ckeditor/ckeditor5-basic-styles/src/italic' { export default cl
 declare module '@ckeditor/ckeditor5-basic-styles/src/strikethrough' { export default class Strikethrough {} }
 declare module '@ckeditor/ckeditor5-basic-styles/src/underline' { export default class Underline {} }
 declare module '@ckeditor/ckeditor5-core/src/plugin' { export default class Plugin {} }
-declare module '@ckeditor/ckeditor5-remove-format/src/removeformat' { export default class RemoveFormat {} }
-declare module '@ckeditor/ckeditor5-remove-format/src/removeformatui' { export default class RemoveFormatLinks {} }
+declare module '@ckeditor/ckeditor5-editor-classic/src/classiceditor' { export default class ClassicEditor {} }
 declare module '@ckeditor/ckeditor5-essentials/src/essentials' { export default class Essentials {} }
 declare module '@ckeditor/ckeditor5-font/src/fontbackgroundcolor' { export default class FontBackgroundColor {} }
 declare module '@ckeditor/ckeditor5-font/src/fontcolor' { export default class FontColor {} }
@@ -23,5 +20,7 @@ declare module '@ckeditor/ckeditor5-media-embed/src/mediaembed' { export default
 declare module '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar' { export default class MediaEmbedToolbar {} }
 declare module '@ckeditor/ckeditor5-paragraph/src/paragraph' { export default class Paragraph {} }
 declare module '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice' { export default class PasteFromOffice {} }
+declare module '@ckeditor/ckeditor5-remove-format/src/removeformat' { export default class RemoveFormat {} }
+declare module '@ckeditor/ckeditor5-remove-format/src/removeformatui' { export default class RemoveFormatLinks {} }
 declare module '@ckeditor/ckeditor5-table/src/table' { export default class Table {} }
 declare module '@ckeditor/ckeditor5-table/src/tabletoolbar' { export default class TableToolbar {} }
