3c0f18c825bce78eb8c1a4e4ded922fd92e76dd0
Cédric Belin <cedric@belin.io>
2019-08-29 14:45:35 +0200

    Updated the CKEditor typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,26 +1,133 @@
-declare module '@ckeditor/ckeditor5-alignment/src/alignment' { export default class Alignment {} }
-declare module '@ckeditor/ckeditor5-autoformat/src/autoformat' { export default class Autoformat {} }
-declare module '@ckeditor/ckeditor5-basic-styles/src/bold' { export default class Bold {} }
-declare module '@ckeditor/ckeditor5-basic-styles/src/italic' { export default class Italic {} }
-declare module '@ckeditor/ckeditor5-basic-styles/src/strikethrough' { export default class Strikethrough {} }
-declare module '@ckeditor/ckeditor5-basic-styles/src/underline' { export default class Underline {} }
-declare module '@ckeditor/ckeditor5-core/src/plugin' { export default class Plugin {} }
-declare module '@ckeditor/ckeditor5-editor-classic/src/classiceditor' { export default class ClassicEditor {} }
-declare module '@ckeditor/ckeditor5-essentials/src/essentials' { export default class Essentials {} }
-declare module '@ckeditor/ckeditor5-font/src/fontbackgroundcolor' { export default class FontBackgroundColor {} }
-declare module '@ckeditor/ckeditor5-font/src/fontcolor' { export default class FontColor {} }
-declare module '@ckeditor/ckeditor5-heading/src/heading' { export default class Heading {} }
-declare module '@ckeditor/ckeditor5-image/src/image' { export default class Image {} }
-declare module '@ckeditor/ckeditor5-image/src/imagecaption' { export default class ImageCaption {} }
-declare module '@ckeditor/ckeditor5-image/src/imagestyle' { export default class ImageStyle {} }
-declare module '@ckeditor/ckeditor5-image/src/imagetoolbar' { export default class ImageToolbar {} }
-declare module '@ckeditor/ckeditor5-link/src/link' { export default class Link {} }
-declare module '@ckeditor/ckeditor5-list/src/list' { export default class List {} }
-declare module '@ckeditor/ckeditor5-media-embed/src/mediaembed' { export default class MediaEmbed {} }
-declare module '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar' { export default class MediaEmbedToolbar {} }
-declare module '@ckeditor/ckeditor5-paragraph/src/paragraph' { export default class Paragraph {} }
-declare module '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice' { export default class PasteFromOffice {} }
-declare module '@ckeditor/ckeditor5-remove-format/src/removeformat' { export default class RemoveFormat {} }
-declare module '@ckeditor/ckeditor5-remove-format/src/removeformatui' { export default class RemoveFormatLinks {} }
-declare module '@ckeditor/ckeditor5-table/src/table' { export default class Table {} }
-declare module '@ckeditor/ckeditor5-table/src/tabletoolbar' { export default class TableToolbar {} }
+/* eslint-disable no-duplicate-imports */
+declare module '@ckeditor/ckeditor5-alignment/src/alignment' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Alignment extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-autoformat/src/autoformat' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Autoformat extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-basic-styles/src/bold' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Bold extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-basic-styles/src/italic' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Italic extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-basic-styles/src/strikethrough' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Strikethrough extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-basic-styles/src/underline' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Underline extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-core/src/editor/editor' {
+  export default class Editor {}
+}
+
+declare module '@ckeditor/ckeditor5-core/src/plugin' {
+  export default class Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-editor-classic/src/classiceditor' {
+  import Editor from '@ckeditor/ckeditor5-core/src/editor/editor';
+  export default class ClassicEditor extends Editor {}
+}
+
+declare module '@ckeditor/ckeditor5-essentials/src/essentials' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Essentials extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-font/src/fontbackgroundcolor' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class FontBackgroundColor extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-font/src/fontcolor' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class FontColor extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-heading/src/heading' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Heading extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-image/src/image' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Image extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-image/src/imagecaption' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class ImageCaption extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-image/src/imagestyle' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class ImageStyle extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-image/src/imagetoolbar' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class ImageToolbar extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-link/src/link' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Link extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-list/src/list' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class List extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-media-embed/src/mediaembed' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class MediaEmbed extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class MediaEmbedToolbar extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-paragraph/src/paragraph' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Paragraph extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class PasteFromOffice extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-remove-format/src/removeformat' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class RemoveFormat extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-remove-format/src/removeformatui' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class RemoveFormatLinks extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-table/src/table' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class Table extends Plugin {}
+}
+
+declare module '@ckeditor/ckeditor5-table/src/tabletoolbar' {
+  import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+  export default class TableToolbar extends Plugin {}
+}
