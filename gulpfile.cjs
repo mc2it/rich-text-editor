df5b67376499fce0375f4d180d94cadbdac593ac
Cédric Belin <cedric@belin.io>
2020-01-14 01:34:37 +0100

    Updated the TypeDoc settings

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -32,7 +32,7 @@ task('clean', () => del(['build', 'doc/api', 'lib', 'var/**/*', 'web']));
 /** Builds the documentation. */
 task('doc', async () => {
   for (const path of ['CHANGELOG.md', 'LICENSE.md']) await promises.copyFile(path, `doc/about/${path.toLowerCase()}`);
-  await _exec('typedoc', ['--gaID', process.env.GOOGLE_ANALYTICS_ID, '--options', 'etc/typedoc.json', '--tsconfig', 'src/tsconfig.json']);
+  await _exec('typedoc', ['--gaID', process.env.GOOGLE_ANALYTICS_ID, '--options', 'etc/typedoc.json']);
   await _exec('mkdocs', ['build', '--config-file=doc/mkdocs.yaml']);
   return del(['doc/about/changelog.md', 'doc/about/license.md', 'web/mkdocs.yaml']);
 });
