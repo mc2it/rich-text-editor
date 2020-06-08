4b48ab97b60e752ecf99f4fb52889f3d2f7a0451
Cédric Belin <cedric@belin.io>
2020-06-08 11:09:26 +0200

    Updated the build system

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -6,6 +6,4 @@ foreach ($item in "build", "doc/api", "lib/editor.*", "www") {
 	if (Test-Path $item) { Remove-Item $item -Recurse }
 }
 
-foreach ($item in Get-ChildItem var -Exclude .gitkeep) {
-	Remove-Item $item -Recurse
-}
+Get-ChildItem var -Exclude .gitkeep | Remove-Item -Recurse
