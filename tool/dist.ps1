4a92eddb0e9292f677db846836b42aeea56dec2d
Cédric Belin <cedric@belin.io>
2020-06-08 14:59:20 +0200

    Updated the build system

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -2,8 +2,9 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-$Env:NODE_ENV = "production"
 tool/clean.ps1
 tool/version.ps1
-haxe --no-traces build.hxml
-node_modules/.bin/webpack --config=etc/webpack.js
+
+$Env:NODE_ENV = "production"
+haxe build.hxml
+npm run-script build
