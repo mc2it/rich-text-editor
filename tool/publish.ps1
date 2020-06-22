00d8d247a384a8088dfbb1fb7a1fbb9f2b6b37a0
Cédric Belin <cedric@belin.io>
2020-06-22 16:19:04 +0200

    Removed support for GitHub package registry

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -3,8 +3,7 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 tool/dist.ps1
-npm publish --registry=https://npm.pkg.github.com
-npm publish --registry=https://registry.npmjs.org
+npm publish
 
 $version = (Get-Content haxelib.json | ConvertFrom-Json).version
 git tag "v$version"
