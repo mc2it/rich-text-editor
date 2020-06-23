c948b88354fc86fd84fe965975d6f358d9634270
Cédric Belin <cedric@belin.io>
2020-06-23 14:39:55 +0200

    Fixed the "clean" task

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -3,7 +3,7 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 foreach ($item in "build", "doc/api", "lib/editor.*", "www") {
-	if (Test-Path $item) { Remove-Item $item -Recurse }
+	if (Test-Path $item) { Remove-Item $item -Force -Recurse }
 }
 
-Get-ChildItem var -Exclude .gitkeep | Remove-Item -Recurse
+Get-ChildItem var -Exclude .gitkeep | Remove-Item -Force -Recurse
