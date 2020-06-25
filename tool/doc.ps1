ea2dde66a57467b11ab9ceb73833c4e09f3fdd35
Cédric Belin <cedric@belin.io>
2020-06-25 17:30:34 +0200

    Fixed the documentation

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -7,7 +7,7 @@ haxe --define doc-gen --xml var/api.xml build.hxml
 haxelib run dox `
 	--define description "A customized build of CKEditor, a rich text editor." `
 	--define logo "https://www.sabcomputer.com/favicon.ico" `
-	--define source-path "https://github.com/sab-international/rich-text-editor/blob/master/src" `
+	--define source-path "https://github.com/sab-international/rich-text-editor/blob/main/src" `
 	--define themeColor 0x4051b5 `
 	--define version $version `
 	--define website "https://www.sabcomputer.com" `
