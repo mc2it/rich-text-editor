9dbc698c94832c4210e0bfb493efdab4557e8c01
Cédric Belin <cedric@belin.io>
2020-06-25 17:18:18 +0200

    Updated the dependencies

--- tool/upgrade.ps1
+++ tool/upgrade.ps1
@@ -2,10 +2,6 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-git reset --hard
-git fetch --all --prune
-git pull --rebase
-
 haxelib install all --always
 haxelib update --always
 npm install --ignore-scripts --production=false
