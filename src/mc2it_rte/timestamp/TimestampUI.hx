0baa99a889a91f09d97b42fa51dc4a8263c41025
Cédric Belin <cedric@belin.io>
2020-11-04 22:18:17 +0100

    Added the `timestamp` UI layer

---
+++ src/mc2it_rte/timestamp/TimestampUI.hx
@@ -0,0 +1,12 @@
+package mc2it_rte.timestamp;
+
+import js.npm.ckeditor.core.Plugin;
+
+/** The user interface layer of the timestamp plugin. **/
+@:keep class TimestampUI extends Plugin {
+
+	/** Initializes this plugin. **/
+	public function init() {
+
+	}
+}
