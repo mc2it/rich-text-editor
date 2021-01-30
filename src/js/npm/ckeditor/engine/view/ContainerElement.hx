803f08145b5a34cc6f224e3ae8b5e4bd79b659c5
Cédric Belin <cedric@belin.io>
2021-01-30 18:29:04 +0100

    Fixed a typo

--- src/js/npm/ckeditor/engine/view/ContainerElement.hx
+++ src/js/npm/ckeditor/engine/view/ContainerElement.hx
@@ -1,5 +1,5 @@
 package js.npm.ckeditor.engine.view;
 
-/** A containers element which define the document structure. **/
+/** A container element which define the document structure. **/
 @:jsRequire("@ckeditor/ckeditor5-engine/src/view/containerelement.js", "default")
 extern class ContainerElement extends Element {}
