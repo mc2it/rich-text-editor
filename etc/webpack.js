30f22d616e427351f3d5e7d47393fc533ed797d7
Cédric Belin <cedric@belin.io>
2021-03-23 12:04:46 +0100

    Updated the build system

--- etc/webpack.js
+++ etc/webpack.js
@@ -4,7 +4,7 @@ const {join} = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-	entry: join(__dirname, "../lib/index.js"),
+	entry: join(__dirname, "../var/editor.js"),
 	mode: process.env.NODE_ENV,
 	module: {
 		rules: [
