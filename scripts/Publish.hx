427e544322f35242f394372562feae9200263eff
Cédric Belin <cedric@belin.io>
2021-07-05 10:41:06 +0200

    Updated the dependencies

--- scripts/Publish.hx
+++ scripts/Publish.hx
@@ -8,7 +8,5 @@ function main() {
 	command("lix Dist");
 	compress(["CHANGELOG.md", "LICENSE.md", "README.md", "haxelib.json", "lib", "run.n", "src"], "var/haxelib.zip");
 	command("haxelib submit var/haxelib.zip");
-
-	final tag = 'v${getPackageVersion()}';
-	for (action in ["tag", "push origin"]) command('git $action $tag');
+	for (action in ["tag", "push origin"]) command('git $action v${getPackageVersion()}');
 }
