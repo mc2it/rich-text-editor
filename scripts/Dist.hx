ff01b2244bc1b757dff059a109494696ab64411a
Cédric Belin <cedric@belin.io>
2021-10-06 08:39:45 +0200

    Fixed the packaging

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -3,5 +3,5 @@ import Sys.*;
 /** Runs the script. **/
 function main() {
 	for (script in ["Clean", "Version", "Build"]) command('lix $script');
-	command("npx terser --comments=false --config-file=etc/terser.json --output=lib/mc2it_rte.min.js lib/mc2it_rte.js");
+	command("npx terser --comments=false --config-file=etc/terser.json --output=lib/mc2it_rte.js lib/mc2it_rte.js");
 }
